66dcb57ed896241d8f1a9223147cd9230ff459de
voodoocoder <andreas.braun.de@gmail.com>
2014-05-28 20:04:36 +0200

    new file:   .gitattributes 	new file:   .gitignore 	new file:   404.php 	new file:   about.php 	new file:   archive.php 	new file:   article.php 	new file:   category.php

---
+++ about.php
@@ -0,0 +1,62 @@
+<div id="aboutme-container" class="cf">
+    <div class="tabs">
+        <ul>
+            <li class="active">
+                <a href="#tab1">Beschreibung</a>
+            </li>
+            <li>
+                <a href="#tab2">Information</a>
+            </li>
+            <li>
+                <a href="#tab3">Software</a>
+            </li>
+            <li>
+                <a href="#tab4">Arbeitszeit</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div id="tab1" class="tab active">
+                <img src="<?php bloginfo('template_directory'); ?>/img/meavatar.png" style="float:left;width:400px;margin-right:20px;max-width:30%"/>
+                <p>
+                    Ein paar allgemeine Worte von mir:
+                </p>
+            </div>
+            <div id="tab2" class="tab" style="margin-left:10px">
+                <p>
+                    Tab #2 content goes here!
+                </p>
+                <p>
+                    Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum risus ornare mollis. In hac habitasse platea dictumst. Ut euismod tempus hendrerit. Morbi ut adipiscing nisi. Etiam rutrum sodales gravida! Aliquam tellus orci, iaculis vel.
+                </p>
+            </div>
+            <div id="tab3" class="tab" style="margin-left:10px">
+                <p>
+                    Tab #3 content goes here!
+                </p>
+                <p>
+                    Donec pulvinar neque sed semper lacinia. Curabitur lacinia ullamcorper nibh; quis imperdiet velit eleifend ac. Donec blandit mauris eget aliquet lacinia! Donec pulvinar massa interdum ri.
+                </p>
+            </div>
+            <div id="tab4" style="margin-left:10px" class="tab">
+                <p>
+                    Zeiterfassung
+                </p>
+                <p>
+                    Eine ungefähre Auflistung der verschiedenen Arbeitsschritte
+                </p>
+                <div class="bar-wrap">
+                    <div class="bar blue" data-time="12.7"></div>
+                </div>
+                <div class="bar-wrap">
+                    <div class="bar red" data-time="5.7"></div>
+                </div>
+                <div class="bar-wrap">
+                    <div class="bar green" data-time="11.3"></div>
+                </div>
+                <div class="bar-wrap">
+                    <div class="bar purple" data-time="5.9"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
